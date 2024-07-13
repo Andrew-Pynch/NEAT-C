27e9a5bcf5ef29dbb85c7b51bdae22741e807e31
Andrew Pynch <andrewpynchbusiness@gmail.com>
2024-07-13 10:13:08 -0500

    fix warnings and desc

--- game.c
+++ game.c
@@ -1,45 +1,9 @@
 /*
- * NEAT-C
- * Copyright (c) 2023 Andrew Pynch
+ * NEAT-C is a C implementation of the NEAT algorithm, a genetic algorithm for
+ * evolving NN toppologies.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *
- * NEAT-C is a C implementation of the NEAT algorithm, a genetic algorithm
- * inspired by the principles of natural selection and genetic programming.
- * It is designed to be used in computer games, where the goal is to create
- * artificial intelligence that can learn and adapt to the game's environment.
- *
- * This is a custom environment for the NEAT algorithm, where predators compete
- * for food. The predators are represented by circles, and the food is
- * represented by a green square. The goal is to keep the predators from eating
- * the food, while avoiding collisions with other predators.
- *
- * The NEAT algorithm is a genetic algorithm inspired by the principles of
- * natural selection and genetic programming. It is designed to be used in
- * computer games, where the goal is to create artificial intelligence that can
- * learn and adapt to the game's environment.
- *
- * The NEAT algorithm is a genetic algorithm inspired by the principles of
- * natural selection and genetic programming. It is designed to be used in
- * computer games, where the goal is to create artificial intelligence that can
- * learn and adapt to the game's environment.
+ * This includes a custom environment for the NEAT algorithm, where predators
+ * compete for natural resources in an evolving environment.
  */
 #include "game.h"
 
